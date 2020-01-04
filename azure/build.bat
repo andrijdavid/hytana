b291a1c886719221a4967d9c30066aaf59689208
andrijdavid <andrijdavid@gmail.com>
2020-01-05 00:18:27 +0100

    correct 2

--- azure/build.bat
+++ azure/build.bat
@@ -140,8 +140,6 @@ for %%v in (%DESIRED_PYTHON_PREFIX%) do (
         set CUDNN_VERSION=7
     )
     if ERRORLEVEL 1 exit /b 1
-    if "%BUILD_PYTHONLESS%" == "" call internal\test.bat
-    if ERRORLEVEL 1 exit /b 1
     @endlocal
 )
 
