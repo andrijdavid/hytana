5829ff6b855aa876f544e878542928a515a495c3
andrijdavid <andrijdavid@gmail.com>
2020-01-05 20:31:08 +0100

    fix pipeline

--- azure/build.bat
+++ azure/build.bat
@@ -1,3 +1,10 @@
 dir
-conda activate py37
+:: Activate Python Environment
+set PYTHON_PREFIX=3.7
+set "CONDA_LIB_PATH=%CONDA_HOME%\envs\%%v\Library\bin"
+if not "%ADDITIONAL_PATH%" == "" (
+    set "PATH=%ADDITIONAL_PATH%;%CONDA_HOME%\envs\%%v;%CONDA_HOME%\envs\%%v\scripts;%CONDA_HOME%\envs\%%v\Library\bin;%ORIG_PATH%"
+) else (
+    set "PATH=%CONDA_HOME%\envs\%%v;%CONDA_HOME%\envs\%%v\scripts;%CONDA_HOME%\envs\%%v\Library\bin;%ORIG_PATH%"
+)
 python setup.py build
